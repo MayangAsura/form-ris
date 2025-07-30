9bca16ca8228924d4051c8ea2d06e14d9d1677ae
MayangAsura <mayang.asura123@gmail.com>
2025-07-30 15:31:27 +0700

    change local to ngrok

--- src/api/local-server.js
+++ src/api/local-server.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL: 'https://1a447cf69a79.ngrok-free.app'
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://1a447cf69a79.ngrok-free.app'
+    // baseURL: 'http://localhost:3000'
 })
