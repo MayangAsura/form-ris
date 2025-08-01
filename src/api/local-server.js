345b6e4c0d70f687098ce1128e77fd778e8ad8e5
MayangAsura <mayang.asura123@gmail.com>
2025-08-01 07:38:28 +0700

    modified source server

--- src/api/local-server.js
+++ src/api/local-server.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://cc8ad2e122f0.ngrok-free.app'
-    // baseURL: 'http://localhost:3000'
+    // baseURL: 'https://cc8ad2e122f0.ngrok-free.app'
+    baseURL: 'http://localhost:3000'
 })
