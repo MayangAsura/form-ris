aa0b3fff97cde1286559935c3e26986b6df6167d
MayangAsura <mayang.asura123@gmail.com>
2025-08-01 07:35:51 +0700

    modified logout axios 2

--- src/api/local-server.js
+++ src/api/local-server.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://4d3f894d7bb5.ngrok-free.app'
+    baseURL: 'https://cc8ad2e122f0.ngrok-free.app'
     // baseURL: 'http://localhost:3000'
 })
