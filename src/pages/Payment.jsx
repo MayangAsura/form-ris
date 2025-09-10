4c121c53fb0991f08ba2bb35f50e3cc5017def82
MayangAsura <mayang.asura123@gmail.com>
2025-09-10 14:11:42 +0700

    updated button

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -665,7 +665,7 @@ function Payment() {
                 )}
                   <div className="flex flex-wrap -mx-3 mt-6">
                     <div className="w-full px-3">
-                      {((applicantData.applicant_orders[0]?.status=='pending') && applicantData.applicant_schools[0]?.applicant_id)? (
+                      {((applicantData.applicant_orders.length===0 || applicantData.applicant_orders[0]?.status=='pending') && applicantData.applicant_schools[0]?.applicant_id)? (
                           <button className="btn text-white bg-green-700 hover:bg-green-600 w-full"
                             onClick={create_order}
                           >Buat Transaksi
@@ -685,7 +685,7 @@ function Payment() {
                           ):(
                             <button className="btn text-white bg-green-700 hover:bg-green-600 w-full"
                               onClick={()=> window.location.href=applicantDataPayment.payment_url}
-                          >Bayar {applicantData.applicant_orders[0]?.status}
+                          >Bayar
                           <svg className="w-3 h-3 fill-current text-white-400 flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                             <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
                           </svg></button>
